846d514affe91db55da316f43eb67f2d0cb24e36
Pedro Quintas <pedroquintas1213@gmail.com>
2023-11-29 16:12:54 -0300

    php

--- front/telas/agendamento.php
+++ front/telas/agendamento.php
@@ -1,33 +1,31 @@
 <?php
-include "../../../front/conexao.php";
+// Obter dados do formulário
 session_start();
+include "../../../front/conexao.php";
 
 $horario = $_POST['horario'];
 $idPaciente = $_SESSION['id_paciente'];
-$id_medico = isset($_POST['id_medico']) ? $_POST['id_medico'] : null;
+$id_medico = $_POST['id_medico'];
 
-if ($id_medico === null) {
-    echo "Erro ao agendar: Id do médico não definido.";
+// Verificar se o horário está disponível
+$sql = "SELECT id FROM projetophp.agendamentos WHERE horario = '$horario' AND medico_id = $id_medico AND disponivel != 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Horário já agendado, faça o tratamento adequado (ex: exiba uma mensagem de erro)
+    $message = "Horário já agendado. Escolha outro horário.";
 } else {
-    // Verificar se o horário está disponível
-    $sql = "SELECT id FROM projetophp.agendamentos WHERE horario = '$horario' AND disponivel = true";
-    $result = $conn->query($sql);
+    // Horário disponível, agendar
+    $sql = "INSERT INTO projetophp.agendamentos (medico_id, paciente_id, horario, disponivel) VALUES ($id_medico, $idPaciente, '$horario', 0 )";
 
-    if ($result->num_rows > 0) {
-        // Horário já agendado, faça o tratamento adequado (ex: exiba uma mensagem de erro)
-        $message = "Horário já agendado. Escolha outro horário.";
+    if ($conn->query($sql) === TRUE) {
+        $message = "Agendamento realizado com sucesso!";
     } else {
-        // Horário disponível, agendar
-        $sql = "INSERT INTO projetophp.agendamentos (medico_id, paciente_id, horario, disponivel) VALUES ('$id_medico', '$idPaciente', '$horario', false)";
-        
-        if ($conn->query($sql) === TRUE) {
-            $message = "Agendamento realizado com sucesso!";
-        } else {
-            $message = "Erro ao agendar: " . $conn->error;
-        }
+        $message = "Erro ao agendar: " . $conn->error;
     }
 }
 
+// Redirecionar de volta à página do médico com a mensagem
 header("Location: telapaciente.php?message=" . urlencode($message));
 exit();
 ?>
--- front/telas/telamedico.php
+++ front/telas/telamedico.php
@@ -22,11 +22,16 @@ verificarLogin();
 
 // Verificar se o usuário é um paciente
 verificarPermissao('medico');
+
+$id_medico = $_SESSION['id_medico'];
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <!-- Seu código de cabeçalho aqui -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela do Médico</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
 <body>
     <section class="section">
@@ -34,7 +39,35 @@ verificarPermissao('medico');
             <h1 class="title">Pacientes e Prontuários</h1>
             <div class="columns">
                 <div class="column is-one-third">
-                    <?php include "consultas_agendadas.php"; ?>
+                    <?php
+                    include "../../../front/conexao.php";
+
+                    $sql = "SELECT c.id, c.horario, p.nome_completo AS nome_paciente, m.nomeSobrenome AS nome_medico
+                            FROM projetophp.agendamentos AS c
+                            INNER JOIN projetophp.pacientes AS p ON c.paciente_id = p.id_paciente
+                            INNER JOIN projetophp.medicos AS m ON c.medico_id = m.id_medico where medico_id = $id_medico ";
+
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<div class='box'>";
+                            echo "<h2 class='subtitle'>Consulta #" . $row['id'] . ": " . $row['nome_paciente'] . "</h2>";
+                            echo "<p>Horário: " . $row['horario'] . "</p>";
+                            echo "<p>Atendido pelo Dr. " . $row['nome_medico'] . "</p>";
+                            echo "<p>Prontuário:</p>";
+                            echo "<div class='content' name='prontuario'>";
+                            echo "<textarea class='textarea' placeholder='Editar Prontuário do " . $row['nome_paciente'] . "...'></textarea>";
+                            echo "<button onclick='prontuariosalvo()'>Visualizar Prontuário</button>";
+                            echo "</div>";
+                            echo "</div>";
+                        }
+                    } else {
+                        echo "<p>Não há consultas agendadas.</p>";
+                    }
+
+                    $conn->close();
+                    ?>
                 </div>
             </div>
         </div>
@@ -47,7 +80,7 @@ verificarPermissao('medico');
         }
 
         function prontuariosalvo() {
-            window.location.href = "prontuario.html"
+            window.location.href = "prontuario.html";
         }
     </script>
 </body>
